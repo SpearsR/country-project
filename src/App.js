d005d2a264bd1b979bc40e51e06d9dc1cce35c98
SpearsR <18rspears@gmail.com>
2021-04-10 13:44:09 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Nav from './Nav';
 import search_icon from './loupe.svg';
+import back_arrow from './left-arrow.svg'
 
-function CountryCard(props){
-  return(
-    <div className="country-card">
-      <div className="img-box" style={{backgroundImage: `url(${props.data.flag})`}}></div>
+function CountryCard(props) {
+  return (
+    <div onClick={ () => props.func(props.data)} className="country-card">
+      <div className="img-box" style={{ backgroundImage: `url(${props.data.flag})` }}></div>
       <div className="country-info">
         <h3>{props.data.name}</h3>
         <span>Population: {props.data.population}</span>
@@ -16,9 +18,9 @@ function CountryCard(props){
   );
 }
 
-function CountryInfo(props){
-  return(
-    <div className="country-detail flex-center">
+function CountryInfo(props) {
+  return (
+    <div id="remove-country" className="country-detail flex-center">
       <img src={props.data.flag} alt="country flag"></img>
       <div className="more-info">
         <h3>{props.data.name}</h3>
@@ -37,101 +39,121 @@ function CountryInfo(props){
           </div>
         </div>
       </div>
-      
+
     </div>
   );
 }
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      data : [],
-      region : '',
-      input : '',
-      selectedData : []
+      data: [],
+      region: '',
+      input: '',
+      selectedData: []
     }
 
     this.getAPIData = this.getAPIData.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.filterChange = this.filterChange.bind(this);
+    this.goInDepth  = this.goInDepth.bind(this);
+    this.goBack = this.goBack.bind(this);
   }
 
-  getAPIData(){
+  getAPIData() {
     fetch(`https://restcountries.eu/rest/v2/name/${this.state.input}`)
       .then(res => res.text())
       .then(data => {
         try {
           data = JSON.parse(data);
-          
+
           //Filter by region
-          if(this.state.region !== ''){
+          if (this.state.region !== '') {
             let dataFilter = []
             data.forEach(element => {
-              if(element.region === this.state.region){
+              if (element.region === this.state.region) {
                 dataFilter.push(element);
               }
             });
             data = dataFilter;
-          } 
+          }
 
-          this.setState({data: data});
+          this.setState({ data: data });
           console.log(data);
         }
-        
+
         catch {
           console.log('error');
-          this.setState({data: []});
+          this.setState({ data: [] });
         }
+
+        var countries;
+        if(data.length > 0){
+          try {
+            countries = this.state.data.map((data) => <CountryCard func={this.goInDepth} key={data.name} data={data} />);
+          }
+          catch {
+            countries = <p id="none-found">No Countries Were Found</p>
+          }
+        }
+        else {
+          countries = <p id="none-found">No Countries Were Found</p>
+        }
+        ReactDOM.render(countries, document.getElementById("countries"));
       })
 
+      
 
-  
   }
 
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({
       input: event.target.value
     });
   }
 
-  filterChange(event){
+  filterChange(event) {
     this.setState({
       region: event.target.value
     });
 
   }
 
-  componentDidMount(){
-    this.getAPIData();
-    console.log(this.state.data);
-    console.log('mounted')
+  goInDepth(data){
+    document.getElementById('main-screen').style.display = "none";
+    document.getElementById('more-detail').style.display = "block";
+    ReactDOM.render(<CountryInfo data={data}/>, document.getElementById('place-data'))
+  }
+  
+  goBack(){
+    document.getElementById('more-detail').style.display = "none";
+    document.getElementById('main-screen').style.display = "block";
+    ReactDOM.unmountComponentAtNode(document.getElementById('remove-country'));
   }
 
-  moreInfo(data){
-    this.setState({
-      selectedData : data
-    });
-
-
+  componentDidMount() {
+    console.log('mounted')
   }
 
 
   render() {
     console.log('render');
-    var countries;
-    try{
-      countries = this.state.data.map((data) => <CountryCard onClick={this.moreInfo(data)} key={data.name} data={data}/>);
-    }
-    catch {
-      countries = <p id="none-found">No Countries Were Found</p>
-    }
 
     return (
       <div className="App">
         <Nav />
-          <div id="main-screen" className="search-container">
+        <div id="more-detail">
+          <div onClick={this.goBack} id='back-button'>
+            <img src={back_arrow} alt="go back"></img> 
+            <span>Back</span>
+          </div>
+          <div id="place-data">
+
+          </div>
+        </div>
+        <div id="main-screen" className="search-container">
           <div id="search-data" className="flex-center">
             <div id="input-canvas" className="flex-center">
               <img onClick={this.getAPIData} src={search_icon} alt="search icon"></img>
@@ -148,10 +170,9 @@ class App extends React.Component {
             </select>
           </div>
           <div id="countries" className="countries">
-            {countries}
           </div>
         </div>
-      </div> 
+      </div>
     );
 
   }
--- src/Nav.js
+++ src/Nav.js
@@ -2,7 +2,6 @@ export default function Nav(){
     return(
         <nav className="flex-center">
             <span>Where in the world?</span>
-            <span className="theme-button">Dark Theme</span>
         </nav>
     );
 }
